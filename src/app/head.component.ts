a73041eba7d700416d3bece943b2fc12b8a7cb24
fan <fan@fandeMacBook-Pro.local>
2017-11-05 23:07:03 +0800

    导航优化，iconfont引用

--- src/app/head.component.ts
+++ src/app/head.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
     <div class="ali_cont">
       <div el-row>
         <div el-col span="9">
-          <div class="ali_logo">雄才联盟</div>
+          <div class="ali_logo" (click)="goPage('/main')">雄才联盟</div>
           <div class="ds_inline">
             <p>中国IT人才摇篮</p>
             <p>Cradle of IT Talent In China</p>
@@ -16,17 +16,17 @@ import { Router } from '@angular/router';
         </div>
         <div el-col span="12">
           <div *ngIf="isMenu">
-            <el-menu [model]="0" class="el-menu-demo" mode="horizontal">
-              <el-menu-item index="1">首 页</el-menu-item>
-              <el-menu-item index="2">招 聘</el-menu-item>
-              <el-menu-item index="3">项 目</el-menu-item>
-              <el-menu-item index="4">培 训</el-menu-item>
+            <el-menu [model]="router.url" class="el-menu-demo" mode="horizontal">
+              <el-menu-item *ngFor="let item of menuList" [index]="item.url"><a [routerLink]="item.url">{{item.name}}</a></el-menu-item>
             </el-menu>
           </div>
         </div>
         <div el-col span="3">
+        <p class="user_seting" *ngIf="!isMenu">
+          <a (click)="goPage('/main')">返回首页</a>
+        </p>
           <p class="user_seting" *ngIf="isMenu">
-            <a (click)="goPage('/register')">注册</a> | <a (click)="goPage('/login')">登录</a>
+            <a (click)="goPage('/register')">注 册</a> | <a (click)="goPage('/login')">登 录</a>
           </p>
         </div>
       </div>
@@ -43,6 +43,7 @@ import { Router } from '@angular/router';
 export class HeadComponent implements OnInit {
   title = '处理中心';
   isMenu: boolean;
+  menuList: any;
   constructor(
     private router: Router
   ) {
@@ -54,6 +55,12 @@ export class HeadComponent implements OnInit {
         scope.isMenu = true;
       }
     }, 600);
+    scope.menuList = [
+      {name: '首 页', url: '/main'},
+      {name: '招 聘', url: '/zhaopin'},
+      {name: '项 目', url: '/object'},
+      {name: '培 训', url: '/train'}
+    ];
   }
   ngOnInit() {}
   goPage (url: string): void {
