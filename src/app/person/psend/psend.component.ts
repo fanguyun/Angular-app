5600ffe7edd43a9bf5bec8895ea1c57a110ceb67
fan <fanzg1045@13322.com>
2017-12-18 11:29:57 +0800

    个人中心-职位投递记录

--- src/app/person/psend/psend.component.ts
+++ src/app/person/psend/psend.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./psend.component.css']
 })
 export class PsendComponent implements OnInit {
-
+  private tableData: any[] =  new Array(20).fill({
+    name: '用户研究',
+    sex: '男',
+    compony: '腾讯计算机有限公司',
+    money: '15000',
+    time: '2017-08-19',
+    address: '上海市普陀区金沙江路 1518 弄',
+    state: '投递成功'
+  })
   constructor() { }
 
   ngOnInit() {
