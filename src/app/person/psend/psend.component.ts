710b446f815d7bcb257d30c20d96c55105201389
fan <fanzg1045@13322.com>
2017-12-21 09:57:05 +0800

    组件优化

--- src/app/person/psend/psend.component.ts
+++ src/app/person/psend/psend.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./psend.component.css']
 })
 export class PsendComponent implements OnInit {
-  private tableData: any[] =  new Array(20).fill({
+  tableData: any[] =  new Array(20).fill({
     name: '用户研究',
     sex: '男',
     compony: '腾讯计算机有限公司',
