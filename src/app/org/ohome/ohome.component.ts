971b3598fa4a0f115987dd791e0555fd7bef88e3
樊峥国 <fanzhg1045@13322ty.com>
2018-01-18 15:48:36 +0800

    企业管理

--- src/app/org/ohome/ohome.component.ts
+++ src/app/org/ohome/ohome.component.ts
@@ -6,12 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ohome.component.css']
 })
 export class OhomeComponent implements OnInit {
-
+  imageUrl: String;
+  orgText: String;
+  orgGuimo: String;
+  orgXinzhi: String;
+  orgHangye: String;
+  orgAddres: String;
+  orgLink: String;
   constructor() { }
   handleMaolink(name): void {
     let offTop = document.getElementById(name).offsetTop
     document.body.scrollTop = document.documentElement.scrollTop = offTop
     // this.router.navigate(['presume'],{ fragment: name })
+  }
+  successHandle(file: any): void {
+    this.imageUrl = URL.createObjectURL(file.raw)
   }
   ngOnInit() {
   }
--- src/app/org/osend/osend.component.ts
+++ src/app/org/osend/osend.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./osend.component.css']
 })
 export class OsendComponent implements OnInit {
-
+  tableData = new Array(20).fill({
+    name: '前端开发',
+    date: '2017-10-12',
+    num: '5',
+    money: '面议',
+    yoer: '3',
+    education: '不限'
+  })
   constructor() { }
 
   ngOnInit() {
+  }
+  handleMaolink(name):void {
+    console.log(name)
   }
 
 }
